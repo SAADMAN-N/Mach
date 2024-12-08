a49ee0930903bef46b084dac5fcbf1e132b2e712
Sharfuzzaman Noor Sadman <saadman.mt@gmail.com>
2024-12-08 14:00:00 -0500

    Add Q&A interface with streaming AI responses

---
+++ src/app/(protected)/dashboard/ask-question-card.tsx
@@ -0,0 +1,135 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import MDEditor from "@uiw/react-md-editor";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { Textarea } from "@/components/ui/textarea";
+import useProject from "@/hooks/use-project";
+import Image from "next/image";
+
+import React, { useState } from "react";
+import { askQuestions } from "./actions";
+import { readStreamableValue } from "@ai-sdk/rsc";
+import CodeReferences from "./code-references";
+import { api } from "@/trpc/react";
+import { toast } from "sonner";
+import useRefetch from "@/hooks/use-refetch";
+
+const AskQuestionCard = () => {
+  const { project } = useProject();
+  const [question, setQuestion] = useState("");
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [fileReferences, setFileReferences] =
+    useState<{ fileName: string; summary: string; sourceCode: string }[]>();
+  const [answer, setAnswer] = useState("");
+  const saveAnswer = api.project.saveAnswer.useMutation();
+
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    setAnswer("");
+    setFileReferences([]);
+    if (!project?.id) return;
+    e.preventDefault();
+    setLoading(true);
+
+    const { output, fileReferences } = await askQuestions(question, project.id);
+    setOpen(true);
+    setFileReferences(fileReferences);
+
+    for await (const delta of readStreamableValue(output)) {
+      if (delta) {
+        setAnswer(delta);
+      }
+    }
+    setLoading(false);
+  };
+
+  const refetch = useRefetch();
+
+  return (
+    <>
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogContent className="sm:max-w-[80vw]">
+          <DialogHeader>
+            <div className="flex items-center gap-2">
+              <DialogTitle>
+                <Image
+                  src={"/mach.svg"}
+                  alt="mach logo"
+                  width={40}
+                  height={40}
+                />
+              </DialogTitle>
+              <Button
+                variant={"outline"}
+                disabled={saveAnswer.isPending}
+                onClick={() => {
+                  saveAnswer.mutate(
+                    {
+                      projectId: project?.id,
+                      question,
+                      answer,
+                      filesReferenced: fileReferences,
+                    },
+                    {
+                      onSuccess: () => {
+                        toast.success("Answer Saved!");
+                        refetch();
+                      },
+                      onError: () => {
+                        toast.error("Failed to save answer");
+                      },
+                    },
+                  );
+                }}
+              >
+                Save Answer
+              </Button>
+            </div>
+          </DialogHeader>
+          <MDEditor.Markdown
+            source={answer}
+            className="h-full max-h-[40vh] max-w-[70vw] overflow-scroll"
+          />
+          <div className="h-4"></div>
+          <CodeReferences fileReferences={fileReferences} />
+
+          <Button
+            type="button"
+            onClick={() => {
+              setOpen(false);
+            }}
+          ></Button>
+        </DialogContent>
+      </Dialog>
+
+      <Card className="relative col-span-3">
+        <CardHeader>
+          <CardTitle>Ask a Question</CardTitle>
+        </CardHeader>
+
+        <CardContent>
+          <form onSubmit={onSubmit}>
+            <Textarea
+              placeholder="Which file should I edit to change the home page?"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+            ></Textarea>
+            <div className="h-4"></div>
+            <Button type="submit" disabled={loading}>
+              Ask Mach
+            </Button>
+          </form>
+        </CardContent>
+      </Card>
+    </>
+  );
+};
+
+export default AskQuestionCard;
