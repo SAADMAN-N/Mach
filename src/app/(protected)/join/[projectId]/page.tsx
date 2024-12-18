f70c9e80b600631a6a2f860822760e571c77f24a
Sharfuzzaman Noor Sadman <saadman.mt@gmail.com>
2024-12-18 10:00:00 -0500

    Add project invitation system

---
+++ src/app/(protected)/join/[projectId]/page.tsx
@@ -0,0 +1,48 @@
+import { db } from "@/server/db";
+import { auth, clerkClient } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import React from "react";
+
+type Props = {
+  params: Promise<{ projectId: string }>;
+};
+
+const JoinHandler = async (props: Props) => {
+  const { projectId } = await props.params;
+  const { userId } = await auth();
+  if (!userId) return redirect("/sign-in");
+  const dbUser = await db.user.findUnique({
+    where: { id: userId },
+  });
+
+  const client = await clerkClient();
+  const user = await client.users.getUser(userId);
+
+  if (!dbUser) {
+    await db.user.create({
+      data: {
+        id: userId,
+        emailAddress: user.emailAddresses[0]!.emailAddress,
+        imageUrl: user.imageUrl,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+    });
+  }
+
+  const project = await db.project.findUnique({ where: { id: projectId } });
+  if (!project) {
+    return redirect("/dashboard");
+  }
+  try {
+    await db.userToProject.create({
+      data: { userId, projectId },
+    });
+  } catch (error) {
+    console.log("\u001b[1;31m User already in project!!");
+  }
+  console.log("\u001b[1;31m Redirecting!!");
+  return redirect(`/dashboard`);
+};
+
+export default JoinHandler;
