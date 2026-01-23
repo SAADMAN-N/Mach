3df69b5bfe33867f726fe7dc780e2af041971494
Sharfuzzaman Noor Sadman <saadman.mt@gmail.com>
2026-01-23 07:31:51 -0500

    fixed Home to redirect to /dashboard upon sign in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,8 @@ import Link from "next/link";
 import { LatestPost } from "@/app/_components/post";
 import { api, HydrateClient } from "@/trpc/server";
 import { Button } from "@/components/ui/button";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  return (
-    <Button>
-      <Link href={"/dashboard"}>Click Me</Link>
-    </Button>
-  );
+  return redirect("/dashboard")
 }
